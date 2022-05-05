d787eb46ea6516c8eb30ae29c4bb3a57fe63e11a
Elnaz <elnazamia@yahoo.com>
2022-05-05 15:10:25 +0200

    Added footer with GitHub and Netlify links

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
-
-import './App.css';
+import "./App.css";
 
 export default function App() {
   return (
     <div className="App">
-     
+      <div className="container">
+        <footer>
+          This project was coded by Elnaz Amia and is{" "}
+          <a
+            href="https://github.com/ElnazAmia/weather-react-app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open-sourced on GitHub
+          </a>{" "}
+          and{" "}
+          <a
+            href="https://fanciful-mochi-71d178.netlify.app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            hosted on Netlify
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
-
-
