c522059ef5caffa36074fbfb418a5a7a71a10725
Elnaz <elnazamia@yahoo.com>
2022-05-25 12:35:04 +0430

    Updated forecast

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import WeatherForecastDay from "./WeatherForecastDay";
 import "./WeatherForecast.css";
 import axios from "axios";
@@ -6,6 +6,10 @@ import axios from "axios";
 export default function WeatherForecast(props) {
   let [loaded, setLoaded] = useState(false);
   let [forecast, setForecast] = useState(null);
+
+  useEffect(() => {
+    setLoaded(false);
+  }, [props.coordinates]);
 
   function handleResponse(response) {
     console.log(response.data);
@@ -16,17 +20,18 @@ export default function WeatherForecast(props) {
 
   if (loaded) {
     return (
-      <div className="weatherForecast">
+      <div className="WeatherForecast">
         <div className="row">
-          {forecast.map(function(dailyForecast,index){
-            if(index<6){
-              return(<div className="col">
-              <WeatherForecastDay data={dailyForecast}/>
-          </div>)
+          {forecast.map(function(dailyForecast, index) {
+            if (index < 6) {
+              return (
+                <div className="col" key={index}>
+                  <WeatherForecastDay data={dailyForecast} />
+                </div>
+              );
             }
-          })
-          }
-          
+            return null;
+          })}
         </div>
       </div>
     );
@@ -36,5 +41,6 @@ export default function WeatherForecast(props) {
     let apiKey = "f0ff64afa8957098b6eda5ad96796c19";
     let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
     axios.get(apiUrl).then(handleResponse);
+    return null;
   }
 }
